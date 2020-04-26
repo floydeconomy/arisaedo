fd41bbc33058f313b845cb7744e77d12323177c2
Jeevan Pillay <jeevan.pillay8@gmail.com>
2020-04-26 15:53:33 +1000

    added kv db functions and interface

---
+++ internal/db/kv/store.go
@@ -0,0 +1,25 @@
+package kv
+
+// Getter defines the method to read the kv store
+type Getter interface {
+	Get(key []byte) ([]byte, error)
+	Has(key []byte) (bool, error)
+}
+
+// Putter defines the method to put into kv store
+type Putter interface {
+	Put(key, value []byte) error
+	Delete(key []byte) error
+}
+
+// Pair defines the key-value persistent pair
+type Pair interface {
+	Key() []byte
+	Value() []byte
+}
+
+// Store defines the functional implementation of a legitatimate key-value store
+type Store interface {
+	Getter
+	Putter
+}
